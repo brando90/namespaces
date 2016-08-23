7e3935867081d076fb757778c79596a53a72fb5f
Pedro Cattori <pcattori@gmail.com>
2016-08-23 19:58:42 -0400

    bracket-access enabled

--- namespaces/namespace.py
+++ namespaces/namespace.py
@@ -1,38 +1,12 @@
 from __future__ import unicode_literals
-import keyword
-from namespaces import isidentifier
 
 class Namespace(dict):
   '''A dictionary with attributes instead of keys.
   The Namespace API is inspired by collections.namedtuple.
-  Attributes provided as strings must be valid python identifiers.
-  Access via __getitem__ and __setitem__ (ie. `namespace[key]`) is disabled.
   '''
 
   def __init__(self, *args, **kwargs):
-    d = dict(*args, **kwargs)
-    for k in d:
-      if not isinstance(k, str) and not isinstance(k, unicode):
-        raise TypeError(
-          "Field names must be {} or {}, not {}: {}".format(
-            str, unicode, type(k), repr(k)
-          )
-        )
-      if not isidentifier(k):
-        raise ValueError("Field name is not a valid identifier: {}".format(repr(k)))
-      if keyword.iskeyword(k):
-        raise ValueError("Field name cannot be a python keyword: {}".format(repr(k)))
-    dict.__init__(self, d)
-
-  def __getitem__(self, name):
-    raise AttributeError(
-      "'{}' object has no attribute '__getitem__'".format(type(self).__name__)
-    )
-
-  def __setitem__(self, name, value):
-    raise AttributeError(
-      "'{}' object has no attribute '__setitem__'".format(type(self).__name__)
-    )
+    dict.__init__(self, dict(*args, **kwargs))
 
   def __getattr__(self, name):
     '''Behaves similarly to collections.namedtuple#__getattr__.'''
@@ -49,8 +23,8 @@ class Namespace(dict):
   def __repr__(self):
     '''Representation is a valid python expression for creating a Namespace
     (assuming contents also implement __repr__ as valid python expressions).'''
-    kwargs_strs = ['{}={}'.format(k,repr(v)) for k,v in self.iteritems()]
-    return '{}({})'.format(type(self).__name__, ', '.join(kwargs_strs))
+    items = ('{}={}'.format(k,repr(v)) for k,v in self.iteritems())
+    return '{}({})'.format(type(self).__name__, ', '.join(items))
 
   def __eq__(self, other):
     return isinstance(other, type(self)) and dict.__eq__(self, other)

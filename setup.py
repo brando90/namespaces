72647b71d33c331303af8136aee87535a8ae75ae
Pedro Cattori <pcattori@gmail.com>
2016-08-23 19:59:10 -0400

    major version bump fixed repo name in url

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
   name='namespaces',
-  version='1.0.0',
+  version='2.0.0',
   description='Python dictionaries with attributes instead of keys',
-  url='https://github.com/pcattori/namespace',
+  url='https://github.com/pcattori/namespaces',
   author='Pedro Cattori',
   author_email='pcattori@gmail.com',
   license='MIT',

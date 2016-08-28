a8d58a278cf545fd438aa6f3a58d8d5404eb676c
Pedro Cattori <pcattori@gmail.com>
2016-08-27 21:06:48 -0400

    clean up Namespace tests

--- test/Namespace.py
+++ test/Namespace.py
@@ -1,50 +1,60 @@
 import unittest
 
-from namespaces import Namespace
+from namespaces import FrozenNamespace, Namespace
 
 class NamespaceTest(unittest.TestCase):
 
   def test_create_empty(self):
-    foo = Namespace()
-    self.assertTrue(True)
+    ns = Namespace()
+    self.assertIsInstance(ns, Namespace)
+    self.assertEqual(len(ns), 0)
 
   def test_create_via_kwargs(self): # anonymous mapping
-    foo = Namespace(a=1, b=2)
-    self.assertTrue(True)
+    ns = Namespace(a=1, b=2)
+    self.assertIsInstance(ns, Namespace)
+    self.assertEqual(ns.items(), {'a': 1, 'b': 2}.items())
 
   def test_create_via_dict(self): # named mapping
-    foo = Namespace({'a': 1, 'b': 2, tuple('three'): 3, 4: 4})
-    self.assertTrue(True)
+    d = {'a': 1, 'b': 2, tuple('three'): 3, 4: 4}
+    ns = Namespace(d)
+    self.assertIsInstance(ns, Namespace)
+    self.assertEqual(ns.items(), d.items())
 
-# TODO: test creation from another namespace (frozen?)
+  def test_create_via_frozennamespace(self):
+    fn = FrozenNamespace(a=1, b=2)
+    ns = Namespace(fn)
+    self.assertIsInstance(ns, Namespace)
+    self.assertEqual(ns.items(), fn.items())
 
   def test_item_setget(self):
-    foo = Namespace()
-    foo['c'] = 3
-    self.assertEqual(foo['c'], 3)
+    ns = Namespace()
+    ns['c'] = 3
+    self.assertEqual(ns['c'], 3)
 
   def test_attr_setget(self):
-    foo = Namespace()
-    foo.c = 3
-    self.assertEqual(foo.c, 3)
+    ns = Namespace()
+    ns.c = 3
+    self.assertEqual(ns.c, 3)
 
   def test_len(self):
-    foo = Namespace(a=1, b=2)
-    self.assertEqual(len(foo), 2)
-    foo['c'] = 3
-    self.assertEqual(len(foo), 3)
-    foo.d = 4
-    self.assertEqual(len(foo), 4)
+    ns = Namespace(a=1, b=2)
+    self.assertEqual(len(ns), 2)
+    ns['c'] = 3
+    self.assertEqual(len(ns), 3)
+    ns.d = 4
+    self.assertEqual(len(ns), 4)
 
   def test_iter(self):
-    foo = Namespace(a=1, b=2)
-    foo_dict = {k: v for k,v in foo.iteritems()}
-    self.assertEqual(foo_dict, {'a': 1, 'b': 2})
+    ns = Namespace(a=1, b=2)
+    d = {k: v for k,v in ns.iteritems()}
+    self.assertEqual(d, {'a': 1, 'b': 2})
 
   def test_del(self):
-    foo = Namespace(a=1, b=2)
-    del foo['a']
-    self.assertNotIn('a', foo)
+    ns = Namespace(a=1, b=2)
+    self.assertEqual(len(ns), 2)
+    del ns['a']
+    self.assertNotIn('a', ns)
+    self.assertEqual(len(ns), 1)
 
 if __name__ == '__main__':
   unittest.main()

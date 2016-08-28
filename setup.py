807098e232de9cd320987dd6ff4c3b27c9bca2ba
Pedro Cattori <pcattori@gmail.com>
2016-08-27 21:11:56 -0400

    major version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='namespaces',
-  version='2.0.0',
+  version='3.0.0',
   description='Python dictionaries with attributes instead of keys',
   url='https://github.com/pcattori/namespaces',
   author='Pedro Cattori',
